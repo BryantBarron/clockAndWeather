fda1f2eb36d83a1d4d90a843d41a3e277a870a66
BryantBarron <bryant.barron.95@my.csun.edu>
2020-02-28 15:38:06 -0800

    first commit after key incident

---
+++ javascript/main.js
@@ -0,0 +1,81 @@
+/*jshint esversion: 6 */
+var myKey = config.key1;
+
+getDate = () => {
+  const dates = new Date();
+  const day = dates.getDate();
+  const hour = dates.getHours();
+  const minutes = dates.getMinutes();
+  const seconds = dates.getSeconds();
+  const year = dates.getFullYear();
+  const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  const dayOfTheWeek = ["Sunday", "Monday","Tuesday","WednesDay","Thursday","Friday","Saturday"];
+
+  if(hour >= 12) {
+    var nonMilitaryTime = hour - 12;
+    if(nonMilitaryTime < 10){
+      console.log(nonMilitaryTime);
+      document.querySelector('.d-hour').innerHTML = ("0" + nonMilitaryTime);
+    }
+  } else {
+    document.querySelector('.d-hour').innerHTML = hour;
+  }
+  if(minutes < 10){
+    document.querySelector('.d-minutes').innerHTML = ("0" + minutes);
+  } else {
+    document.querySelector('.d-minutes').innerHTML = minutes;
+  }
+
+  if(seconds < 10){
+    document.querySelector('.d-seconds').innerHTML = ("0" + seconds);
+  } else {
+    document.querySelector('.d-seconds').innerHTML = seconds;
+  }
+  document.querySelector('#month').innerHTML = month[dates.getMonth()];
+  document.querySelector('#day').innerHTML = day;
+  document.querySelector('#year').innerHTML = year;
+  document.querySelector('#dayOfWeek').innerHTML = dayOfTheWeek[dates.getDay()];
+};
+
+setInterval(getDate, 1000);
+
+window.addEventListener('load', () => {
+  let long;
+  let lat;
+  let temperatureDescription = document.querySelector('.temperature-description');
+  let temperatureDegree = document.querySelector('.temperature-degree');
+  let locationTimeZone = document.querySelector('.location-timeZone');
+  console.log("hello");
+
+  if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(position => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+
+      const proxy = "https://cors-anywhere.herokuapp.com/";
+      const api = `${proxy}https://api.darksky.net/forecast/${myKey}/${lat},${long}`;
+
+      fetch(api).then(data =>{
+        return data.json();
+      }).then(data => {
+        console.log(data);
+        const {temperature, summary, icon} = data.currently;
+        
+        temperatureDegree.textContent = temperature;
+        temperatureDescription.textContent = summary;
+        locationTimeZone.textContent = data.timezone;
+
+        setIcons(icon, document.querySelector('.icon'));
+      });
+    });
+  }else{
+    console.log("This doesnt work");
+  }
+
+  function setIcons(icon, iconID){
+    const skycons = new Skycons({color: "white"});
+    const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+    skycons.play();
+    return skycons.set(iconID, Skycons[currentIcon]);
+  }
+});
